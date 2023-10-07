3b87d93d5fe8c97ccd6955e138cedd4a21e933f7
muaga <more9494@naver.com>
2023-10-07 15:46:11 +0900

    일러스트까지 아완료

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:clone_login_app_blue/color.dart';
+import 'package:clone_login_app_blue/size.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +7,32 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(main_gap),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Selamat Datang di Steplife",
+                  style: TextStyle(fontSize: medium_font, color: kDarkGray)),
+              SizedBox(height: medium_gap),
+              Text(
+                "Perlindungan dan ketentraman finansial anda",
+                style: TextStyle(
+                    fontSize: xxlarge_font, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: large_gap),
+              Align(
+                child: Image.asset(
+                  "assets/together.png",
+                  height: 300,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
